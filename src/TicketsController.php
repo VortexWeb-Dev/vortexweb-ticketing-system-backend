f91d0af86457b305ad104fcd0a5c83176cd8157b
fasilofficial <muhammedfasilofficial@gmail.com>
2025-04-24 15:35:27 +0530

    Disable caching for tickets

--- src/TicketsController.php
+++ src/TicketsController.php
@@ -85,13 +85,13 @@ class TicketsController extends BitrixController
 
     public function processRequest(string $method, ?string $id): void
     {
-        $cacheKey = $method . '_' . ($_GET['endpoint'] ?? 'tickets') . ($id ?? 'all') . (isset($_GET['page']) ? '_page_' . $_GET['page'] : '');
+        // $cacheKey = $method . '_' . ($_GET['endpoint'] ?? 'tickets') . ($id ?? 'all') . (isset($_GET['page']) ? '_page_' . $_GET['page'] : '');
 
-        $cachedData = $this->cache->get($cacheKey);
-        if ($cachedData !== null && $method === 'GET') {
-            $this->response->sendSuccess(200, $cachedData);
-            return;
-        }
+        // $cachedData = $this->cache->get($cacheKey);
+        // if ($cachedData !== null && $method === 'GET') {
+        //     $this->response->sendSuccess(200, $cachedData);
+        //     return;
+        // }
 
         try {
             switch ($method) {
@@ -125,7 +125,7 @@ class TicketsController extends BitrixController
             }
 
             if ($method === 'GET') {
-                $this->cache->set($cacheKey, $data);
+                // $this->cache->set($cacheKey, $data);
             } else {
                 $this->invalidateCache();
             }
